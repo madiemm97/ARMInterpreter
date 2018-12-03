0f40a93168ea98bbf02bd2d6d233f2119226b84d
Madie Martens <madiemm97@gmail.com>
2018-12-03 17:54:08 -0600

    Class 11/29

--- app/src/main/java/com/example/madie/arminterpreter/Instruction.java
+++ app/src/main/java/com/example/madie/arminterpreter/Instruction.java
@@ -1,11 +1,11 @@
 package com.example.madie.arminterpreter;
+
 import java.util.LinkedList;
 
 public class Instruction
 {
     private String magicTrick;
-    private String destinationName;
-    private LinkedList<String> theInputNames = new LinkedList<String>();
+    private String[] parameters;
 
     public Instruction(String instruction)
     {
@@ -14,50 +14,47 @@ public class Instruction
         this.magicTrick = instruction.substring(0, locationOfFirstSpace);
 
         String theRest = instruction.substring(locationOfFirstSpace).trim(); //"X0,[X1, 4]"
-        String[] parts = theRest.split(","); // ["X0", "[X1", "4]"]
-        this.destinationName = parts[0].trim(); //"X0
-        this.theInputNames.add(parts[1].trim()); //"X1"
-        this.theInputNames.add(parts[2].trim()); //"X2"
+        this.parameters = theRest.split(","); // ["X0", "[X1", "4]"]
     }
 
     public void execute()
     {
         if(this.magicTrick.equalsIgnoreCase("ADD"))
         {
-            Register destinationRegister = ARMap.findRegisterWithName(this.destinationName);
-            Register input2Register = ARMap.findRegisterWithName(theInputNames.get(0));
-            Register input1Register = ARMap.findRegisterWithName(theInputNames.get(1));
+            Register destinationRegister = ARMap.findRegisterWithName(this.parameters[0]);
+            Register input2Register = ARMap.findRegisterWithName(this.parameters[1]);
+            Register input1Register = ARMap.findRegisterWithName(this.parameters[2]);
             destinationRegister.setValue(input2Register.getValue() + input1Register.getValue());
         }
         else if(this.magicTrick.equalsIgnoreCase("SUB"))
         {
             //write the code to Subtract input1 from input2 and store the result in destinationName
-            Register destinationRegister = ARMap.findRegisterWithName(this.destinationName);
-            Register input2Register = ARMap.findRegisterWithName(theInputNames.get(0));
-            Register input1Register = ARMap.findRegisterWithName(theInputNames.get(1));
+            Register destinationRegister = ARMap.findRegisterWithName(this.parameters[0]);
+            Register input2Register = ARMap.findRegisterWithName(this.parameters[1]);
+            Register input1Register = ARMap.findRegisterWithName(this.parameters[2]);
             destinationRegister.setValue(input2Register.getValue() - input1Register.getValue());
         }
         else if(this.magicTrick.equalsIgnoreCase("ADDI"))
         {
             //write the code to ADD input2 to an immediate value and store the result in destinationName
-            Register destinationRegister = ARMap.findRegisterWithName(this.destinationName);
-            Register input2Register = ARMap.findRegisterWithName(theInputNames.get(0));
-            int immediateValue = Integer.parseInt(theInputNames.get(1));
+            Register destinationRegister = ARMap.findRegisterWithName(this.parameters[0]);
+            Register input2Register = ARMap.findRegisterWithName(this.parameters[1]);
+            int immediateValue = Integer.parseInt(this.parameters[2]);
             destinationRegister.setValue(input2Register.getValue() + immediateValue);
         }
         else if(this.magicTrick.equalsIgnoreCase("SUBI"))
         {
             //write the code to Subtract the immediate value in input1 from input2 and store the result in destinationName
-            Register destinationRegister = ARMap.findRegisterWithName(this.destinationName);
-            Register input2Register = ARMap.findRegisterWithName(theInputNames.get(0));
-            int immediateValue = Integer.parseInt(theInputNames.get(1));
+            Register destinationRegister = ARMap.findRegisterWithName(this.parameters[0]);
+            Register input2Register = ARMap.findRegisterWithName(this.parameters[1]);
+            int immediateValue = Integer.parseInt(this.parameters[2]);
             destinationRegister.setValue(input2Register.getValue() - immediateValue);
         }
         else if(this.magicTrick.equalsIgnoreCase("LDUR"))
         {
-            Register destinationRegister = ARMap.findRegisterWithName(this.destinationName);
-            Register baseRegister = ARMap.findRegisterWithName(theInputNames.get(0).substring(1));
-            String temp = theInputNames.get(1);
+            Register destinationRegister = ARMap.findRegisterWithName(this.parameters[0]);
+            Register baseRegister = ARMap.findRegisterWithName(this.parameters[1].substring(1));
+            String temp = this.parameters[2];
             int offset = Integer.parseInt(temp.substring(0, temp.length()-1));
             int baseAddressValue = baseRegister.getValue();
             int finalMemoryBucket = baseAddressValue + offset;
@@ -65,14 +62,18 @@ public class Instruction
         }
         else if(this.magicTrick.equalsIgnoreCase("STUR"))
         {
-            Register destinationRegister = ARMap.findRegisterWithName(this.destinationName);
-            Register baseRegister = ARMap.findRegisterWithName(theInputNames.get(0).substring(1));
-            String temp = theInputNames.get(1);
-            int num = Integer.parseInt(temp.substring(0, temp.length()-1));
+            Register sourceRegister = ARMap.findRegisterWithName(this.parameters[0]);
+            Register baseRegister = ARMap.findRegisterWithName(this.parameters[1].substring(1));
+            String temp = this.parameters[2];
+            int offset = Integer.parseInt(temp.substring(0, temp.length()-1));
             int baseAddressValue = baseRegister.getValue();
-            int finalMemoryNum = baseAddressValue + num;
-            destinationRegister.setValue(finalMemoryNum);
-
+            int finalMemoryBucket = baseAddressValue + offset;
+            ARMap.memory[finalMemoryBucket] = sourceRegister.getValue();
+        }
+        else if(this.magicTrick.equalsIgnoreCase("MOVZ"))
+        {
+            Register destinationRegister = ARMap.findRegisterWithName(this.parameters[0]);
+            destinationRegister.setValue(Integer.parseInt(this.parameters[1].trim()));
         }
     }
 }
